adaeb9a56a1a7a8bf9b45bc20a3ac6026fe59385
benisamuel <benisamuel566@gmail.com>
2025-04-12 20:55:32 +0200

    added securityconfig

---
+++ src/main/java/com/lock/lock/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.lock.lock.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
--- src/main/java/com/lock/lock/model/User.java
+++ src/main/java/com/lock/lock/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import com.lock.lock.enums.Role;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
